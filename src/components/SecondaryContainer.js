a8049c34c1868bd6af2f14ca21dafad893340699
Srishruti24 <sri.shruti24@gmail.com>
2025-06-03 15:46:07 +0530

    `feat: Added GPT Search feature, multi-language support and UI changes`

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -7,10 +7,9 @@ const SecondaryContainer = () => {
     <div className=' bg-black'>
       <div className='-mt-52  pl-12 relative z-20'>
       <MovieList title={"Now playing"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Horror"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Kids"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Top Rated"} movies={movies.TopRatedMovies}/>
+      <MovieList title={"Popular"} movies={movies.PopularMovies}/>
+      <MovieList title={"Upcoming"} movies={movies.UpcomingMovies}/>
       </div>
       {/* 
       MovieList- popular
